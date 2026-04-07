8f2698a4884b0c3c94d8760827b5ccc41ad9460f
MUHAMMADULLAH1991 <muhammadullahsaify859658@gmail.com>
2026-04-07 23:18:40 +0600

    Main Website Mastering is Done

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,13 +9,13 @@
                     <div class="home__category-outer">
                         <ul class="header__category-list">
                             <li class="header__category-list-item item-has-submenu">
-                                <a href="category-product.html" class="header__category-list-item-link">
+                                <a href="{{url('/category-products')}}" class="header__category-list-item-link">
                                     <img src="{{ asset('frontend/assets/images/product.png') }}" alt="category">
                                     Test Category
                                 </a>
                                 <ul class="header__nav-item-category-submenu">
                                     <li class="header__category-submenu-item">
-                                        <a href="sub-category-product.html" class="header__category-submenu-item-link">
+                                        <a href="{{url('/subcategory-products')}}" class="header__category-submenu-item-link">
                                             Test Subcategory
                                         </a>
                                     </li>
@@ -43,8 +43,8 @@
                     </h1>
                 </div>
                 <div class="categoris-items-wrapper owl-carousel">
-                    <a href="#" class="categoris-item">
-                        <img src="{{ asset('frontend/assets/images/product.png') }}" alt="category" />
+                    <a href="{{url('/category-products')}}" class="categoris-item">
+                        <img src="{{asset('frontend/assets/images/product.png')}}" alt="category" />
                         <h6 class="categoris-name">
                             Test Category
                         </h6>
@@ -84,7 +84,7 @@
                     <h1 class="title">
                         Hot Products
                     </h1>
-                    <a href="type-products.html" class="product-view-all-btn">
+                    <a href="{{url('/type-products')}}" class="product-view-all-btn">
                         View All
                     </a>
                 </div>
@@ -190,7 +190,7 @@
                     <h1 class="title">
                         New Arrival
                     </h1>
-                    <a href="type-products.html" class="product-view-all-btn">
+                    <a href="{{url('/type-products')}}" class="product-view-all-btn">
                         View All
                     </a>
                 </div>
@@ -296,7 +296,7 @@
                     <h1 class="title">
                         Regular Products
                     </h1>
-                    <a href="type-products.html" class="product-view-all-btn">
+                    <a href="{{url('/type-products')}}" class="product-view-all-btn">
                         View All
                     </a>
                 </div>
@@ -402,7 +402,7 @@
                     <h1 class="title">
                         Discount Products
                     </h1>
-                    <a href="type-products.html" class="product-view-all-btn">
+                    <a href="{{url('/type-products')}}" class="product-view-all-btn">
                         View All
                     </a>
                 </div>
